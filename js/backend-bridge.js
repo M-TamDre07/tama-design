15220dcf6fad1ab11870906f2abba6890e313d18
M-TamDre07 <tamaandrea92@gmail.com>
2026-09-16 09:18:30 +0700

    Clean up backend bridge for new Apps Script deployment

--- js/backend-bridge.js
+++ js/backend-bridge.js
@@ -3,15 +3,6 @@
   'use strict';
   var CURRENT='https://script.google.com/macros/s/AKfycbymlROisZWRIL5754kyzTe59dLoWMYaJ3f_AAz4TbThpluXD1e3tQd5AbQvfJXzGJ7VJg/exec';
   window.TA_BACKEND_URL=CURRENT;
-  if(typeof window.fetch==='function'){
-    var nativeFetch=window.fetch.bind(window);
-    function rewrite(input){
-      if(typeof input==='string') return input;
-      if(window.Request && input instanceof Request) return input;
-      return input;
-    }
-    window.fetch=function(input,init){return nativeFetch(rewrite(input),init)};
-  }
   function loadOrderIntegration(){
     if(!/\/pesan\.html$/.test(location.pathname)||window.__TA_ORDER_INTEGRATION__)return;
     window.__TA_ORDER_INTEGRATION__=true;
